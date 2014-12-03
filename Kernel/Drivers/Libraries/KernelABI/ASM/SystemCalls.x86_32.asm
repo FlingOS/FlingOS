4b85a9dc08385cebf7fa406ebd2763314a055f72
EdNutting <edwardnutting@outlook.com>
2014-12-03 19:00:26 +0000

    Started working on the Drivers framework. Added System Calls class and ASM plug file to KernelABI.
    Former-commit-id: ccc712f90555549657088ad9c3596695d63fa2b7

---
+++ Kernel/Drivers/Libraries/KernelABI/ASM/SystemCalls.x86_32.asm
@@ -0,0 +1,14 @@
+﻿method_System_UInt32_RETEND_KernelABI_SystemCalls_DECLEND_Call_NAMEEND__KernelABI_SystemCalls_Calls_System_UInt32_System_UInt32_System_UInt32_:
+push dword ebp
+mov dword ebp, esp
+
+mov dword eax, [ebp+24]
+mov dword ebx, [ebp+20]
+mov dword ecx, [ebp+16]
+mov dword edx, [ebp+12]
+int eax
+mov dword [ebp+8], eax
+
+
+pop dword ebp
+ret
---
+++ Kernel/Drivers/Libraries/KernelABI/SystemCalls.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KernelABI
+{
+    public static class SystemCalls
+    {
+        public enum Calls : uint
+        {
+            Sleep = 1
+        }
+
+        public static uint Call(Calls callNumber,
+            uint Param1,
+            uint Param2,
+            uint Param3)
+        {
+            return 0;
+        }
+        public static void Sleep(uint ms)
+        {
+            Call(Calls.Sleep, ms, 0, 0, 0);
+        }
+    }
+}
