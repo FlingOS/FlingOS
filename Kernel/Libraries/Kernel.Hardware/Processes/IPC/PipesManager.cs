1004be482f075a62facae112b60391cae1f6d558
EdNutting <edwardnutting@outlook.com>
2014-12-03 04:27:35 +0000

    Corrected a load of project locations. Added PipesManager class.
    Former-commit-id: 787bbf80d214e7e7a1271797a6d2ba41c452096f

---
+++ Kernel/Libraries/Kernel.Hardware/Processes/IPC/PipesManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kernel.Hardware.Processes.IPC
+{
+    public static unsafe class PipesManager
+    {
+    }
+}
