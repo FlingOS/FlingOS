ebf248f1613d7dfa905d16712159a24dfe5c2e13
EdNutting <edwardnutting@outlook.com>
2014-12-08 17:54:49 +0000

    Spec'ed out the structure and execution/control tree of the Driver Compiler. Added VariableInfo class.
    Former-commit-id: 6d69ff190758ef3c0dd02dba3901b0cd54facd97

--- Kernel/Drivers/Compiler/App/CompilerProcess.cs
+++ Kernel/Drivers/Compiler/App/CompilerProcess.cs
@@ -38,7 +38,32 @@ namespace Drivers.Compiler.App
             DateTime startTime = DateTime.Now;
             Logger.LogMessage("", 0, "Driver compiler started  @ " + startTime.ToLongTimeString());
 
+            // IL Library - In a list of libraries returned to the higher-level control app (this app)
+            //              from Library Loader
+            //  : Type Info         - In a list of types in the IL Library
+            //      : Field Info        - In a list of static and non-static fields in the Type Info
+            //      : Method Info       - In a list of methods in the Type Info
+            //          : Variable Info     - In a list of variables in the Method Info
+            //  : IL Block          - In a list of blocks in the IL Library and held against the origin Method Info
+            //      : IL Op             - In a list of ops in the IL Block
+            //
+            // ASM Library - In a list of libraries returned to the higher-level control app (this app)
+            //               from IL Compiler
+            //  : ASM Block         - In a list of blocks in the ASM Library and held against the origin IL Block
+            //      : ASM Op            - In a list of ops in the ASM Block
 
+            // Library loader   - Loads IL Libraries to be compiled
+            //      - Type Scanner     - Loads all the Type Infos, Field Infos and Method Infos
+            // IL Compiler      - Manages the IL compile stage
+            //      - IL Reader        - Loads IL ops from IL Methods in IL Types. Also load plug info.
+            //      - IL Preprocessor  - Pre-scans IL ops to find things like necessary branching labels
+            //                           and check for unchanged methods
+            //      - IL Scanner       - Converts IL ops to ASM ops
+            // ASM Compiler     - Manages the ASM compile stage
+            //      - ASM Preprocessor - Pre-scans the ASM ops to store things like debug info or perform
+            //                           optimisation
+            //      - ASM Processor    - Converts ASM ops into ASM text then runs NASM
+            // Link Manager     - Manages the linker stage. Links together all the NASM outputs using "ld".
 
             DateTime endTime = DateTime.Now;
             Logger.LogMessage("", 0, "Driver compiler finished @ " + endTime.ToLongTimeString());
