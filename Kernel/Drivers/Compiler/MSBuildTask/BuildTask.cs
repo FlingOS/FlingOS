ae9be058642ea10f822ef2c39d49eaa051ea8fce
EdNutting <edwardnutting@outlook.com>
2015-04-04 12:26:26 +0100

    MSBuildTask now reports success/fail properly. Removed old debug code from Kernel_Start.x86_32.asm
    Former-commit-id: cfc6602e3b28ed593b6db0f4cc74f26ea84c6fcb

--- Kernel/Drivers/Compiler/MSBuildTask/BuildTask.cs
+++ Kernel/Drivers/Compiler/MSBuildTask/BuildTask.cs
@@ -76,7 +76,7 @@ namespace Drivers.Compiler.MSBuildTask
             return App.CompilerProcess.Execute(
                     Logger_OnLogMessage,
                     Logger_OnLogWarning,
-                    Logger_OnLogError) != App.CompilerProcess.ErrorCode.NoError;
+                    Logger_OnLogError) == App.CompilerProcess.ErrorCode.NoError;
         }
 
         private void Logger_OnLogError(string errorCode, string file, int lineNumber, string message)
