039c44aa5415bb4529162e029ac34b32b5f1da2a
Roland Baranyi <rb12809@my.bristol.ac.uk>
2015-08-17 12:10:47 +0100

    Implemented 'Stfld' IL op.
    Former-commit-id: bcdf8971b5c8fc29adef633a708b42b174ac60f7

---
+++ Kernel/Drivers/Compiler/Architectures/MIPS32/ILOps/Stfld.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Drivers.Compiler.IL;
+
+namespace Drivers.Compiler.Architectures.MIPS32
+{
+    /// <summary>
+    /// See base class documentation.
+    /// </summary>
+    public class Stfld : IL.ILOps.Stfld
+    {
+        public override void PerformStackOperations(ILPreprocessState conversionState, ILOp theOp)
+        {
+            conversionState.CurrentStackFrame.Stack.Pop();
+            conversionState.CurrentStackFrame.Stack.Pop();
+        }
+
+                /// <summary>
+        /// See base class documentation.
+        /// </summary>
+        /// <param name="theOp">See base class documentation.</param>
+        /// <param name="conversionState">See base class documentation.</param>
+        /// <returns>See base class documentation.</returns>
+        /// <exception cref="System.NotSupportedException">
+        /// Thrown if the value to store is floating point or
+        /// if the value is not 4 or 8 bytes in size.
+        /// </exception>
+        public override void Convert(ILConversionState conversionState, ILOp theOp)
+        {
+             int metadataToken = Utilities.ReadInt32(theOp.ValueBytes, 0);
+            FieldInfo theField = conversionState.Input.TheMethodInfo.UnderlyingInfo.Module.ResolveField(metadataToken);
+            Types.TypeInfo objTypeInfo = conversionState.TheILLibrary.GetTypeInfo(theField.DeclaringType);
+            Types.TypeInfo fieldTypeInfo = conversionState.TheILLibrary.GetTypeInfo(theField.FieldType);
+            Types.FieldInfo theFieldInfo = conversionState.TheILLibrary.GetFieldInfo(objTypeInfo, theField.Name);
+            
+
+            int offset = theFieldInfo.OffsetInBytes;
+
+            int stackSize = fieldTypeInfo.SizeOnStackInBytes;
+            int memSize = fieldTypeInfo.IsValueType ? fieldTypeInfo.SizeOnHeapInBytes : stackSize;
+
+            StackItem value = conversionState.CurrentStackFrame.Stack.Pop();
+            StackItem objPointer = conversionState.CurrentStackFrame.Stack.Pop();
+            
+            if (value.isFloat)
+            {
+                //SUPPORT - floats
+                throw new NotSupportedException("Storing fields of type float not supported yet!");
+            }
+
+            //Get object pointer
+            conversionState.Append(new ASMOps.Mov() { Size = ASMOps.OperandSize.Word, Src = stackSize.ToString() + "$sp", Dest = "$t3" });
+            //Pop and mov value
+            for (int i = 0; i < memSize; i += 2)
+            {
+                if (memSize - i == 1)
+                {
+                    conversionState.Append(new ASMOps.Pop() { Size = ASMOps.OperandSize.Halfword, Dest = "$t0" });
+                    conversionState.Append(new ASMOps.Mov() { Size = ASMOps.OperandSize.Byte, Src = "$t0", Dest = (offset + i).ToString() + "($t3)", MoveType = ASMOps.Mov.MoveTypes.SrcMemoryToDestReg });
+                }
+                else
+                {
+                    conversionState.Append(new ASMOps.Pop() { Size = ASMOps.OperandSize.Halfword, Dest = "$t0" });
+                    conversionState.Append(new ASMOps.Mov() { Size = ASMOps.OperandSize.Halfword, Src = "$t0", Dest = (offset + i).ToString() + "($t3)", MoveType = ASMOps.Mov.MoveTypes.SrcMemoryToDestReg });
+                }
+            }
+            //                                                           Rounds down             || Pop object pointer
+            conversionState.Append(new ASMOps.Add() { Src1 = ((((stackSize - memSize) / 2) * 2) + 4).ToString(), Src2 = "$sp", Dest = "$sp" });
+        }
+    }
+}
