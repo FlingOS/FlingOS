478fa030e680f0b19faf4a02b5d0116ad5a10c38
Roland Baranyi <rb12809@my.bristol.ac.uk>
2015-08-24 19:21:46 +0100

    Implemented 'Sizeof' IL op. Minor corrections.
    Former-commit-id: d9200e52045fff8156c7bd92fed13033db5967c1

--- Kernel/Drivers/Compiler/Architectures/MIPS32/ILOps/Sizeof.cs
+++ Kernel/Drivers/Compiler/Architectures/MIPS32/ILOps/Sizeof.cs
@@ -23,7 +23,7 @@
 //
 // ------------------------------------------------------------------------------ //
 #endregion
-    
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,28 +31,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Drivers.Compiler.IL;
 
-namespace Drivers.Compiler.Architectures.x86
+namespace Drivers.Compiler.Architectures.MIPS32
 {
     /// <summary>
     /// See base class documentation.
     /// </summary>
-    public class Ldtoken : IL.ILOps.Ldtoken
+    public class Sizeof : IL.ILOps.Sizeof
     {
         public override void PerformStackOperations(ILPreprocessState conversionState, ILOp theOp)
         {
-            try
+            conversionState.CurrentStackFrame.Stack.Push(new StackItem()
             {
-                conversionState.CurrentStackFrame.Stack.Push(new StackItem()
-                {
-                    isFloat = false,
-                    sizeOnStackInBytes = 4,
-                    isGCManaged = false
-                });
-            }
-            catch
-            {
-                throw new NotSupportedException("The metadata token specifies a fieldref or methodref which isn't supported yet!");
-            }
+                isFloat = false,
+                sizeOnStackInBytes = 4,
+                isGCManaged = false
+            });
         }
 
         /// <summary>
@@ -61,38 +54,20 @@ namespace Drivers.Compiler.Architectures.x86
         /// <param name="theOp">See base class documentation.</param>
         /// <param name="conversionState">See base class documentation.</param>
         /// <returns>See base class documentation.</returns>
-        /// <exception cref="System.NotSupportedException">
-        /// Thrown when the metadata token is not for method metadata.
-        /// </exception>
         public override void Convert(ILConversionState conversionState, ILOp theOp)
         {
-            //Load token i.e. typeref
-            //It should also support methodref and fieldrefs
-
             int metadataToken = Utilities.ReadInt32(theOp.ValueBytes, 0);
-            try
-            {
-                Type theType = conversionState.Input.TheMethodInfo.UnderlyingInfo.Module.ResolveType(metadataToken);
-                if (theType == null)
-                {
-                    throw new NullReferenceException();
-                }
-                string typeTableId = conversionState.TheILLibrary.GetTypeInfo(theType).ID;
-                conversionState.AddExternalLabel(typeTableId);
+            Type theType = conversionState.Input.TheMethodInfo.UnderlyingInfo.Module.ResolveType(metadataToken);
+            Types.TypeInfo theTypeInfo = conversionState.TheILLibrary.GetTypeInfo(theType);
+            conversionState.Append(new ASMOps.La() { Dest = "$t4", Label = theTypeInfo.SizeOnStackInBytes.ToString() });
+            conversionState.Append(new ASMOps.Push() { Size = ASMOps.OperandSize.Word, Src = "$t4" });
 
-                conversionState.Append(new ASMOps.Push() { Size = ASMOps.OperandSize.Dword, Src = typeTableId });
-                
-                conversionState.CurrentStackFrame.Stack.Push(new StackItem()
-                {
-                    isFloat = false,
-                    sizeOnStackInBytes = 4,
-                    isGCManaged = false
-                });
-            }
-            catch
+            conversionState.CurrentStackFrame.Stack.Push(new StackItem()
             {
-                throw new NotSupportedException("The metadata token specifies a fieldref or methodref which isn't supported yet!");
-            }
+                isFloat = false,
+                sizeOnStackInBytes = 4,
+                isGCManaged = false
+            });
         }
     }
 }
