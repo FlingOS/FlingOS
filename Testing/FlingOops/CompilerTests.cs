7bfcb95b70f0a865107ce55c64a7207d4b6a9b83
Roland Baranyi <rb12809@my.bristol.ac.uk>
2015-08-31 14:41:56 +0100

    Added "Test_ModInt32_9_3()" to CompilerTests.
    Former-commit-id: fa94795b876f8c28df224290110ba212a38f87b4

--- Testing/FlingOops/CompilerTests.cs
+++ Testing/FlingOops/CompilerTests.cs
@@ -61,6 +61,7 @@ namespace FlingOops
             Test_ModInt32_Neg9_3();
             Test_ModInt32_9_Neg3();
             Test_ModInt32_Neg9_Neg3();
+            Test_ModInt32_9_3();
 
             Log.WriteLine("Tests completed.");
         }
@@ -250,6 +251,27 @@ namespace FlingOops
             else
             {
                 Log.WriteError("Test_ModInt32_Neg9_Neg3 not okay.");
+            }
+        }
+
+        /// <summary>
+        /// Tests: Modulus (remainder) operation using signed 32-bit integers, 
+        /// Inputs: 9, 3, 
+        /// Result: 0
+        /// </summary>
+        [NoGC]
+        public static void Test_ModInt32_9_3()
+        {
+            Int32 a = 9;
+            Int32 b = 3;
+            a = a % b;
+            if (a == 0)
+            {
+                Log.WriteSuccess("Test_ModInt32_9_3 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_ModInt32_9_3 not okay.");
             }
         }
     }
