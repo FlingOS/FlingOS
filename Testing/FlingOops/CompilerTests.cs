681cabc0513f38013aa7e99438c8723c2069780d
Roland Baranyi <rb12809@my.bristol.ac.uk>
2015-08-31 15:08:33 +0100

    Added "Test_ModInt32_10_Neg3()" to CompilerTests. Comment correction.
    Former-commit-id: 8560f34bb9c437f7410c6b6ba0e410e28f7c5ae3

--- Testing/FlingOops/CompilerTests.cs
+++ Testing/FlingOops/CompilerTests.cs
@@ -63,6 +63,7 @@ namespace FlingOops
             Test_ModInt32_Neg9_Neg3();
             Test_ModInt32_9_3();
             Test_ModInt32_Neg10_3();
+            Test_ModInt32_10_Neg3();
 
             Log.WriteLine("Tests completed.");
         }
@@ -279,7 +280,7 @@ namespace FlingOops
         /// <summary>
         /// Tests: Modulus (remainder) operation using signed 32-bit integers, 
         /// Inputs: -10, 3, 
-        /// Result: 1
+        /// Result: -1
         /// </summary>
         [NoGC]
         public static void Test_ModInt32_Neg10_3()
@@ -294,6 +295,27 @@ namespace FlingOops
             else
             {
                 Log.WriteError("Test_ModInt32_Neg10_3 not okay.");
+            }
+        }
+
+        /// <summary>
+        /// Tests: Modulus (remainder) operation using signed 32-bit integers, 
+        /// Inputs: 10, -3, 
+        /// Result: 1
+        /// </summary>
+        [NoGC]
+        public static void Test_ModInt32_10_Neg3()
+        {
+            Int32 a = 10;
+            Int32 b = -3;
+            a = a % b;
+            if (a == 1)
+            {
+                Log.WriteSuccess("Test_ModInt32_10_Neg3 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_ModInt32_10_Neg3 not okay.");
             }
         }
     }
