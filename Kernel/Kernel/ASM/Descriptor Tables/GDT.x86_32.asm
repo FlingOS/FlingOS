62fccaac296e5a7b46fc28b1e3c6d0071f8e35f2
EdNutting <edwardnutting@outlook.com>
2015-09-07 14:10:53 +0100

    Updated license headers for new files.

--- Kernel/Kernel/ASM/Descriptor Tables/GDT.x86_32.asm
+++ Kernel/Kernel/ASM/Descriptor Tables/GDT.x86_32.asm
@@ -1,4 +1,27 @@
-﻿BITS 32
+﻿; - - - - - - - - - - - - - - - - - - - LICENSE - - - - - - - - - - - - - - - -  ;
+;
+;    Fling OS - The educational operating system
+;    Copyright (C) 2015 Edward Nutting
+;
+;    This program is free software: you can redistribute it and/or modify
+;    it under the terms of the GNU General Public License as published by
+;    the Free Software Foundation, either version 2 of the License, or
+;    (at your option) any later version.
+;
+;    This program is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU General Public License for more details.
+;
+;    You should have received a copy of the GNU General Public License
+;    along with this program.  If not, see <http:;www.gnu.org/licenses/>.
+;
+;  Project owner: 
+;		Email: edwardnutting@outlook.com
+;		For paper mail address, please contact via email for details.
+;
+; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  ;
+BITS 32
 
 SECTION .text
 GLOBAL File_GDT:function
