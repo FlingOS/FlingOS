dc572bfa8108ebed705dcb389c6dea5e990686d1
Roland Baranyi <rb12809@my.bristol.ac.uk>
2015-09-09 14:08:01 +0100

    Added test cases to test Xor unsigned 64-64.

--- Testing/FlingOops/CompilerTests.cs
+++ Testing/FlingOops/CompilerTests.cs
@@ -5519,6 +5519,7 @@ namespace FlingOops
             Int64 sign64_b;
             Log.WriteLine(" 32-32");
             Log.WriteLine("  Unsigned");
+            
             // 0 ^ 0
             unsign32_a = 0;
             unsign32_b = 0;
@@ -5689,7 +5690,161 @@ namespace FlingOops
 
             Log.WriteLine(" 64-64");
             Log.WriteLine("  Unsigned");
+            // 0 ^ 0
+            unsign64_a = 0;
+            unsign64_b = 0;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0_UInt64_0 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0_UInt64_0 not okay.");
+            }
 
+            // 0 ^ 1
+            unsign64_a = 0;
+            unsign64_b = 1;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 1)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0_UInt64_1 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0_UInt64_1 not okay.");
+            }
+
+            // 1 ^ 0
+            unsign64_a = 1;
+            unsign64_b = 0;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 1)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_1_UInt64_0 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_1_UInt64_0 not okay.");
+            }
+
+            // 1 ^ 1
+            unsign64_a = 1;
+            unsign64_b = 1;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_1_UInt64_1 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_1_UInt64_1 not okay.");
+            }
+
+            // 0xFFFFFFFFFFFFFFFF ^ 0
+            unsign64_a = 0xFFFFFFFFFFFFFFFF;
+            unsign64_b = 0;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0xFFFFFFFFFFFFFFFF)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_0 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_0 not okay.");
+            }
+
+            // 0xFFFFFFFFFFFFFFFF ^ 1
+            unsign64_a = 0xFFFFFFFFFFFFFFFF;
+            unsign64_b = 1;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0xFFFFFFFFFFFFFFFE)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_1 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_1 not okay.");
+            }
+
+            // 0xFFFFFFFFFFFFFFFF ^ 0xFFFFFFFFFFFFFFFF
+            unsign64_a = 0xFFFFFFFFFFFFFFFF;
+            unsign64_b = 0xFFFFFFFFFFFFFFFF;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_0xFFFFFFFFFFFFFFFF okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_0xFFFFFFFFFFFFFFFF not okay.");
+            }
+
+            // 0xFFFFFFFFFFFFFFFF ^ 0x44444444
+            unsign64_a = 0xFFFFFFFFFFFFFFFF;
+            unsign64_b = 0x4444444444444444;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0xBBBBBBBBBBBBBBBB)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_0x4444444444444444 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_0x4444444444444444 not okay.");
+            }
+
+            // 0xFFFFFFFFFFFFFFFF ^ 0x0800080008000800
+            unsign64_a = 0xFFFFFFFFFFFFFFFF;
+            unsign64_b = 0x0800080008000800;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0xF7FFF7FFF7FFF7FF)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_0x0800080008000800 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0xFFFFFFFFFFFFFFFF_UInt64_0x0800080008000800 not okay.");
+            }
+
+            // 0 ^ 0x4444444444444444
+            unsign64_a = 0;
+            unsign64_b = 0x4444444444444444;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0x4444444444444444)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0_UInt64_0x4444444444444444 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0_UInt64_0x4444444444444444 not okay.");
+            }
+
+            // 0 ^ 0x0800080008000800
+            unsign64_a = 0;
+            unsign64_b = 0x0800080008000800;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0x0800080008000800)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0_UInt64_0x0800080008000800 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0_UInt64_0x0800080008000800 not okay.");
+            }
+
+            // 0x0800080008000800 ^ 0x4444444444444444
+            unsign64_a = 0x0800080008000800;
+            unsign64_b = 0x4444444444444444;
+            unsign64_a = unsign64_a ^ unsign64_b;
+            if (unsign64_a == 0x4C444C444C444C44)
+            {
+                Log.WriteSuccess("Test_Xor_UInt64_0x0800080008000800_UInt64_0x4444444444444444 okay.");
+            }
+            else
+            {
+                Log.WriteError("Test_Xor_UInt64_0x0800080008000800_UInt64_0x4444444444444444 not okay.");
+            }
             Log.WriteLine("  Signed");
 
         }
