77c969112293f55ed4cb60fd02e49219f2af5fe3
DaveOsDev <DaveOSDev@outlook.com>
2016-03-24 00:17:19 +0000

    Wouldn't this be better as a timer function rather than nops as the number of nops may change according to the speed of the CPU?

--- Kernel/Libraries/Kernel.Hardware/ASM/CPUs/CPUx86_32/Halt.x86_32.asm
+++ Kernel/Libraries/Kernel.Hardware/ASM/CPUs/CPUx86_32/Halt.x86_32.asm
@@ -5,6 +5,7 @@ SECTION .text
 GLOBAL method_System_Void_RETEND_Kernel_Hardware_CPUs_CPUx86_32_DECLEND_Halt_NAMEEND___:function
 
 method_System_Void_RETEND_Kernel_Hardware_CPUs_CPUx86_32_DECLEND_Halt_NAMEEND___:
+;TODO Wouldn't this be better as a timer function rather than nops as the number of nops may change according to the speed of the CPU?
 ; Nops allow time for interrupts to occur
 nop
 nop
