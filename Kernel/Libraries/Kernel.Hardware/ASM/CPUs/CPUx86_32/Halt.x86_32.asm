1a46d27c766373da3f1af43c27a883f3d07e87d1
EdNutting <edwardnutting@outlook.com>
2016-03-24 13:52:58 +0000

    Removed unnecessary NOPs
    Removed unnecessary NOPs (and confusing comment). Halt operation does allow interrupts, so I suspect this was a fix for an old bug that was incorrectly diagnosed and has since been eliminated.

--- Kernel/Libraries/Kernel.Hardware/ASM/CPUs/CPUx86_32/Halt.x86_32.asm
+++ Kernel/Libraries/Kernel.Hardware/ASM/CPUs/CPUx86_32/Halt.x86_32.asm
@@ -5,26 +5,5 @@ SECTION .text
 GLOBAL method_System_Void_RETEND_Kernel_Hardware_CPUs_CPUx86_32_DECLEND_Halt_NAMEEND___:function
 
 method_System_Void_RETEND_Kernel_Hardware_CPUs_CPUx86_32_DECLEND_Halt_NAMEEND___:
-; Nops allow time for interrupts to occur
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
 hlt
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
 ret
