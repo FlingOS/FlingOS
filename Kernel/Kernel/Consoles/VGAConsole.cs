5277ab863533f3ff0c90ca4b273c425d5ca46087
Edward Nutting <git@flingos.co.uk>
2017-01-29 21:59:43 +0000

    Eliminate duplicate port creation in VGA subsystem.

--- Kernel/Kernel/Consoles/VGAConsole.cs
+++ Kernel/Kernel/Consoles/VGAConsole.cs
@@ -40,16 +40,6 @@ namespace Kernel.Consoles
     public unsafe class VGAConsole : Console
     {
         /// <summary>
-        ///     The command port for manipulating the VGA text-mode cursor.
-        /// </summary>
-        protected IOPort CursorCmdPort = new IOPort(0x3D4);
-
-        /// <summary>
-        ///     The data port for manipulating the VGA text-mode cursor.
-        /// </summary>
-        protected IOPort CursorDataPort = new IOPort(0x3D5);
-
-        /// <summary>
         ///     A pointer to the start of the (character-based) video memory.
         /// </summary>
         protected char* vidMemBasePtr
@@ -157,11 +147,7 @@ namespace Kernel.Consoles
                 //  (not number of bytes).
                 ushort offset = (ushort)(line*ScreenLineWidth + character);
                 //Output the high-byte
-                CursorCmdPort.Write_Byte(14);
-                CursorDataPort.Write_Byte((byte)(offset >> 8));
-                //Output the low-byte
-                CursorCmdPort.Write_Byte(15);
-                CursorDataPort.Write_Byte((byte)offset);
+                TheVGA.UpdateCursorOffset(offset);
             }
         }
 
